19880607d1a3c69f3bfd286917379f44623895da
IREMY250 <ibyishakajeanremy0@gmail.com>
2025-11-13 23:04:35 +0200

    module docs

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """Prints the titles of the first 10 hot posts for a given subreddit."""
 
@@ -5,13 +6,13 @@ import requests
 
 
 def top_ten(subreddit):
-    """Queries Reddit API and prints titles of first 10 hot posts."""
+    """Main function"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    
+    # If the subreddit is invalid (not found or forbidden)
     if response.status_code != 200:
         print(None)
         return
@@ -27,5 +28,5 @@ def top_ten(subreddit):
 
 
 if __name__ == "__main__":
-    
+    # Example usage
     top_ten("python")

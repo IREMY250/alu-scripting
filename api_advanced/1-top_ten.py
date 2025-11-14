12bfad87ae22974f774ebb49f17ce2ad438f84ea
IREMY250 <ibyishakajeanremy0@gmail.com>
2025-11-14 21:34:15 +0200

    solving module

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""Prints the titles of the first 10 hot posts listed for a given subreddit"""
+"""
+Module for querying the Reddit API to retrieve hot posts.
+"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Queries Reddit API and prints the titles of the first 10 hot posts."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    # If not a valid subreddit
     if response.status_code != 200:
-        print("OK")
+        print(None)
         return
 
     posts = response.json().get("data", {}).get("children", [])
     if not posts:
-        print("OK")
+        print(None)
         return
 
     for post in posts:

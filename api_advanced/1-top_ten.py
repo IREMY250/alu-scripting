a93df2308f5c9d12217df60a9ddffd669b024c8f
IREMY250 <ibyishakajeanremy0@gmail.com>
2025-11-14 21:37:15 +0200

    juggling around module docs

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module for querying the Reddit API to retrieve hot posts.
+Module for querying the Reddit API.
 """
 
 import requests
@@ -8,7 +8,7 @@ import requests
 
 def top_ten(subreddit):
     """
-    Prints the titles of the first 10 hot posts for a given subreddit.
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0"}
